550c8943f508718f2d7d8bba9fd222d61d0190bb
JLibeau <jeremie.libeau@egerie.eu>
2023-08-03 15:08:17 +0200

    ; => ,

--- replace.sh
+++ replace.sh
@@ -5,8 +5,8 @@ TRAD="$2"
 
 cp "$TRAD" "$TRAD".tmp
 while read LINE; do
-	SRC=$(echo $LINE | cut -d';' -f 2)
-	TARGET=$(echo $LINE | cut -d';' -f 3)
+	SRC=$(echo $LINE | cut -d',' -f 2)
+	TARGET=$(echo $LINE | cut -d',' -f 3)
   	NEW=$(sed -e "s/$SRC/$TARGET/g" "$TRAD".tmp)
   	echo -e "$NEW" > "$TRAD".tmp
 done < "$CSV"
