48f3e69f8b7ce4afba75ba2f289d403c75a37567
JLibeau <jeremie.libeau@egerie.eu>
2023-08-03 15:58:41 +0200

    Process all .vtt in dir

--- replace.sh
+++ replace.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 CSV="$1"
-TRAD="$2"
+TRADS="$2"
 
-cp "$TRAD" "$TRAD".tmp
-while read LINE; do
-	SRC=$(echo $LINE | cut -d',' -f 2)
-	TARGET=$(echo $LINE | cut -d',' -f 3)
-  	NEW=$(sed -e "s/$SRC/$TARGET/g" "$TRAD".tmp)
-  	echo -e "$NEW" > "$TRAD".tmp
-done < "$CSV"
+if [ -z "$2" ]; then
+	TRADS=$(find . -type f -name '*.vtt')
+fi
+
+for TRAD in $TRADS; do
+	echo -n "Processing $TRAD ..."
+	cp "$TRAD" "$TRAD".tmp
+	while read LINE; do
+		SRC=$(echo $LINE | cut -d';' -f 2)
+		TARGET=$(echo $LINE | cut -d';' -f 3)
+	  	NEW=$(sed -e "s/$SRC/$TARGET/g" "$TRAD".tmp)
+	  	echo -e "$NEW" > "$TRAD".tmp
+	done < "$CSV"
+	echo " OK"
+done
+
